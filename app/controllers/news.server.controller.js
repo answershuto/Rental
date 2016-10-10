67f46e9561ef395ff9e1db729b8b4d0fba9cd00d
answershuto <905307789@qq.com>
2016-10-10 20:14:05 +0800

    去掉route无用代码

--- app/controllers/news.server.controller.js
+++ app/controllers/news.server.controller.js
@@ -1,31 +1,5 @@
 var mongoose = require('mongoose');
-var News = mongoose.model('news');
 
 module.exports = {
-	create: function(req, res, next){
-
-		var news = new News({
-			title: "bbb",
-			content: "content bbb"
-		});
-		
-		news.save(function(err){
-			if (err) {
-				return next(err);
-			};
-
-			res.json(news);
-		});
-
-		//res.send('create!\n');
-	},
-	find: function(req,res,next){
-		News.find({},function(err,doc){
-			if (err) {
-				return next(err);
-			};
-
-			res.json(doc);
-		})
-	}
+	
 }
--- app/models/news.server.model.js
+++ app/models/news.server.model.js
@@ -1,9 +1,1 @@
 var mongoose = require('mongoose');
-
-var Schema = new mongoose.Schema({
-	title: String,
-	content: String,
-	date: {type:Date,default:Date.now}
-})
-
-var News = mongoose.model('news', Schema);
