51d7c741658a5c7ff77d8cd1ed6c8f130afdb5bb
answershuto <905307789@qq.com>
2016-10-19 22:55:49 +0800

    百度地图api根据地点名称获取经纬度信息

--- views/js/index.js
+++ views/js/index.js
@@ -1,6 +1,12 @@
 (function(){
 	var map = new BMap.Map("container");          // 创建地图实例  
-	var point = new BMap.Point(116.404, 39.915);  // 创建点坐标  
-	map.centerAndZoom(point, 15);                 // 初始化地图，设置中心点坐标和地图级别 
+	map.centerAndZoom("杭州", 13);
 	
+	var localSearch = new BMap.LocalSearch(map);
+	var keyword = '浙江大华';
+	localSearch.setSearchCompleteCallback(function (searchResult) {
+		var poi = searchResult.getPoi(0);
+		console.log(poi)
+　　});
+	localSearch.search(keyword);
 })();
